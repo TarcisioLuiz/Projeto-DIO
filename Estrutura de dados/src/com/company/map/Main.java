8ce496c4faa69ec144adde0158996b1f44da084e
Tarcisiosilvahive <tarcisio.silva@hivecloud.com.br>
2022-01-08 18:16:52 -0300

    Final Part of data estructure class

---
+++ Estrutura de dados/src/com/company/map/Main.java
@@ -0,0 +1,30 @@
+package com.company.map;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, String> aluno = new HashMap<>();
+
+        aluno.put("Nome", "João");
+        aluno.put("Idade", "17");
+        aluno.put("Média", "8.5");
+        aluno.put("Turma", "3a");
+
+        System.out.println(aluno);
+
+        System.out.println(aluno.keySet());
+        System.out.println(aluno.values());
+
+        List<Map<String, String>> listaAlunos = new ArrayList<>();
+
+        listaAlunos.add(aluno);
+
+
+    }
+}
